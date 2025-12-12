f86c6467fb32a310f664215111fb46f663e1c859
lier72 <surdu.m@tantive.ru>
2025-12-11 22:06:18 -0500

    [FIX] Fixed statistics output

--- upload/admin/controller/event/statistics.php
+++ upload/admin/controller/event/statistics.php
@@ -2,15 +2,15 @@
 class ControllerEventStatistics extends Controller {
 	// model/catalog/review/removeReview/after
 	public function removeReview(&$route, &$args, &$output) {
-		$this->load->model('setting/statistics');
+		$this->load->model('report/statistics');
 
-		$this->model_report_statistics->addValue('review', 1);	
+		$this->model_report_statistics->removeValue('review', 1);
 	}
-		
+
 	// model/sale/return/removeReturn/after
 	public function removeReturn(&$route, &$args, &$output) {
-		$this->load->model('setting/statistics');
+		$this->load->model('report/statistics');
 
-		$this->model_report_statistics->addValue('return', 1);	
+		$this->model_report_statistics->removeValue('return', 1);
 	}	
 }
