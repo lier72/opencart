ac4945873ff505d5cf8ef150dfaa9ba062b5d71b
lier72 <surdu.m@tantive.ru>
2026-06-01 01:03:35 +0300

    [IMP] AB test vaiant: Product family new UI

--- upload/catalog/language/ru-ru/extension/module/product_family.php
+++ upload/catalog/language/ru-ru/extension/module/product_family.php
@@ -3,5 +3,9 @@
 $_['heading_title']     = 'Варианты';
 
 // Text
-$_['text_select']       = 'Выберите вариант:';
-$_['text_out_of_stock'] = 'Нет в наличии';
+$_['text_select']          = 'Выберите вариант:';
+$_['text_current_variant'] = 'Выбран';
+$_['text_show_more']       = 'Показать еще %s вариантов';
+$_['text_show_less']       = 'Скрыть варианты';
+$_['text_out_of_stock']    = 'Нет в наличии';
+$_['text_sku']             = 'Артикул';
